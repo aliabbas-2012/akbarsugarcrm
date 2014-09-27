c9d283461ecb6c6c848966f086571f9d2b2664c2
ali <ali@192.168.1.3>
2014-09-27 19:41:35 +0500

    email code can revert if need

---
+++ modules/Leads/emailtest.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once('modules/Schedulers/_AddJobsHere.php');
+
+// runMassEmailCampaign
+runMassEmailCampaign();
+die('done');
+?>
